e9becdba6a0c22e58ffea74feb74a3b080d65720
Toby Inkster <mail@tobyinkster.co.uk>
2018-07-28 10:51:39 +0100

    bump version; changelog

--- lib/MooseX/MungeHas.pm
+++ lib/MooseX/MungeHas.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$MooseX::MungeHas::AUTHORITY = 'cpan:TOBYINK';
-	$MooseX::MungeHas::VERSION   = '0.008';
+	$MooseX::MungeHas::VERSION   = '0.009';
 };
 
 use Carp qw(croak);
